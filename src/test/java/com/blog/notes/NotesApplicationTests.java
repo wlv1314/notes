81a5d2a19510290da85fe7649927fc82e132975e
wlv1314 <1245080925@qq.com>
2021-02-04 16:51:14 +0800

    mongo记录转换

--- src/test/java/com/blog/notes/NotesApplicationTests.java
+++ src/test/java/com/blog/notes/NotesApplicationTests.java
@@ -1,16 +1,33 @@
 package com.blog.notes;
 
+import com.blog.crawler.dao.PictureDao;
+import com.blog.crawler.domain.PictureInfo;
 import com.blog.notes.dao.UserDao;
 import com.blog.notes.entity.User;
 import com.blog.notes.service.MailService;
+import com.blog.notes.util.FastDFSClient;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.DigestUtils;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,26 +39,57 @@ public class NotesApplicationTests {
     private MailService mailService;
     @Resource
     private UserDao userDao;
+    @Autowired
+    private FastDFSClient fastDFSClient;
+    @Resource
+    private PictureDao pictureDao;
+    @Value("${IMAGE_SERVER_URL}")
+    private String IMAGE_SERVER_URL;
 
     @Test
     public void contextLoads() {
-        LocalDateTime now = LocalDateTime.now();
-        for(int i=0;i<10000;i++) {
-            List<User> emps=new ArrayList<User>();
-
-            for(int j=0;j<1000;j++) {
-                User emp=new User();
-                emp.setUserName("test"+i+j);
-                emp.setPassword("test"+j);
-                emp.setEmail("11");
-                emps.add(emp);
+    }
+    @Test
+    public void testImgToMysql() {
+        List<PictureInfo> all = pictureDao.findBySiteIsNull();
+        for (int i = 0; i < all.size(); i++) {
+            try {
+                PictureInfo pictureInfo = all.get(i);
+                if(!StringUtils.isEmpty(pictureInfo.getSite())){
+                    continue;
+                }
+                String url=pictureInfo.getSrc();
+                String imageName = url.substring(url.lastIndexOf("/") + 1, url.length());
+                URL uri = new URL(url);
+                // 打开连接
+                URLConnection con = uri.openConnection();
+                //设置请求超时为5s
+                con.setConnectTimeout(5 * 1000);
+                con.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+                // 输入流
+                InputStream is = con.getInputStream();
+                byte[] bytes = IOUtils.toByteArray(is);
+                System.out.println(imageName + ":--下载完成"+pictureInfo.getId());
+                is.close();
+                String extName=imageName.substring(imageName.lastIndexOf(".")+1);
+                String s = fastDFSClient.uploadFile(bytes, extName);
+                String dfsUrl=IMAGE_SERVER_URL+s;
+                pictureInfo.setSite(dfsUrl);
+                pictureInfo.setCreateTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")));
+                pictureDao.save(pictureInfo);
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.out.println("下载错误"+e);
             }
-            int changed=userDao.batchInsert(emps);
-            System.out.println("#"+i+" changed="+changed);
         }
-        LocalDateTime last = LocalDateTime.now();
-        Duration between = Duration.between(now, last);
-        System.out.println("耗时:"+between.toMinutes());
+    }
+
+    @Test
+    public void testFastDFS() throws Exception {
+        File file=new File("C:\\Users\\ldl\\Desktop\\love1.jpg");
+        byte[] bytes = FileUtils.readFileToByteArray(file);
+        String s = fastDFSClient.uploadFile(bytes, "jpg");
+        System.out.println(s);
     }
 
 }
